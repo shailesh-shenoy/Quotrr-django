75e89fb3edc2375adc339b3529c3a385fb970887
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2019-03-23 17:31:45 +0530

    moved api urls to quotes/urls.py

--- quotrr/urls.py
+++ quotrr/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from quotes import views
 
-router = routers.DefaultRouter()
-router.register(r"personality", views.PersonalityViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+    path("", include("quotes.urls")),
     path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
 ]
