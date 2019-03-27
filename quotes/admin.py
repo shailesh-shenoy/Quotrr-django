9fb943e6b126c66c8637221cd20f1611c48aa85c
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2019-03-27 23:24:53 +0530

    Added the Quote model

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Personality
+from .models import Personality, Quote
 
 # Register your models here.
 admin.site.register(Personality)
+admin.site.register(Quote)
--- quotes/models.py
+++ quotes/models.py
@@ -8,7 +8,7 @@ class Personality(models.Model):
     personality_name = models.CharField(
         "Influential Person", unique=True, max_length=100
     )
-    slug = models.SlugField(blank=True, unique=True, max_length=100)
+    slug = models.SlugField("Slug", blank=True, unique=True, max_length=100)
     info = models.TextField("Information")
     trivia = models.TextField("Trivia")
 
@@ -23,3 +23,20 @@ class Personality(models.Model):
 
     class Meta:
         verbose_name_plural = "personalities"
+
+
+class Quote(models.Model):
+    personality = models.ForeignKey(
+        Personality, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    quote_text = models.TextField("Quote")
+    quote_slug = models.SlugField("Slug", blank=True, unique=True, max_length=255)
+
+    def __str__(self):
+        return self.quote_text
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.quote_slug = slugify(self.quote_text)[:255]
+
+        super(Quote, self).save(*args, **kwargs)
